2288f968ccec8444533cb920aed82ec1f26102bb
lskoff <lucasskoff@gmail.com>
2017-10-27 19:04:34 -0500

    Exported hero to class.

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -9,15 +9,24 @@ var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
-        this.hero = 'Windstorm';
+        this.hero = {
+            id: 1,
+            name: 'Windsotrm'
+        };
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>{{title}}</h1><h2>{{hero}} details!</h2>",
+        template: "<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>",
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
 //# sourceMappingURL=app.component.js.map
