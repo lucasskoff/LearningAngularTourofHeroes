b6dc50fd577115ff9253942a351630feea9f9e56
lskoff <lucasskoff@gmail.com>
2017-11-02 13:46:03 -0500

    Adding routing to be able to switch between a dashboard containing the top 4 heroes and the regular hero list.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule            } from '@angular/core';
 import { BrowserModule       } from '@angular/platform-browser';
 import { FormsModule         } from '@angular/forms';
+import { RouterModule        } from '@angular/router';
+
 import { HeroesComponent     } from './heroes.component';
 import { AppComponent        } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService         } from './hero.service';
+import { DashboardComponent  } from './dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])],
+  declarations: [ AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent ],
   bootstrap:    [ AppComponent ],
   providers:    [ HeroService ]
 })
