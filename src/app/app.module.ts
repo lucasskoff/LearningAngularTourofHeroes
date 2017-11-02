7a1287e2b5a25c6470cc90506bf0cf1d19d1eba8
lskoff <lucasskoff@gmail.com>
2017-11-01 22:36:48 -0500

    Refactored further, appcomponent is now purely for navigation or 'routing'.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { NgModule      } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule   } from '@angular/forms';
-import { AppComponent  } from './app.component';
+import { NgModule            } from '@angular/core';
+import { BrowserModule       } from '@angular/platform-browser';
+import { FormsModule         } from '@angular/forms';
+import { HeroesComponent     } from './heroes.component';
+import { AppComponent        } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService         } from './hero.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
+  bootstrap:    [ AppComponent ],
+  providers:    [ HeroService ]
 })
 export class AppModule { }
